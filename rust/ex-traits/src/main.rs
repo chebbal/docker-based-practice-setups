38729865e77c6e923f441af2149a56832eb54ebf
Chaitanya Hebbal <chaitanya.hebbal@neura-robotics.com>
2026-07-29 12:16:07 +0200

    chore: update traits example

--- rust/ex-traits/src/main.rs
+++ rust/ex-traits/src/main.rs
@@ -17,6 +17,7 @@ impl Pet for Dog {
     }
 }
 
+// inheritance vs rust traits
 trait Animal {
     fn sound(&self);
 }
@@ -39,11 +40,34 @@ fn test_trait_2() {
 
 fn test_trait_1() {
     println!("Example - traits 1");
-    let c = Cat{};
-    let d = Dog{};
+    let c = Cat {};
+    let d = Dog {};
     c.speak(); // there is no is-a relationship between cat and dog
     d.speak(); // there is no is-a relationship between cat and dog
-  
+}
+
+// Trait bounds and generic constraints
+// C++ template equivalent (less constrained)
+// template<typename T>
+// T add_and_print(T a, T b) {
+//     // No guarantee T supports + or printing
+//     return a + b;  // Might fail at compile time
+// }
+use std::fmt::Display;
+use std::ops::Add;
+
+//rust explicit trait bounds
+fn add_and_print<T>(a: T, b: T) -> T
+where
+    T: Display + Add<Output = T> + Copy,
+{
+    println!("Adding {} + {}", a, b);
+    a + b // Add trait
+}
+
+fn test_trait_3() {
+    println!("Example -trait bounds");
+    println!("sum: {}",add_and_print(40, 2));
 }
 
 fn main() {
@@ -60,5 +84,6 @@ fn main() {
     println!("{}", "-".repeat(20));
     test_trait_2();
     println!("{}", "-".repeat(20));
-
+    test_trait_3();
+    println!("{}", "-".repeat(20));
 }
