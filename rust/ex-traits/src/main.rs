5cb845067d79ff3f06b952597b54e31d0fc29cfd
Chaitanya Hebbal <chaitanya.hebbal@neura-robotics.com>
2026-07-29 06:57:33 +0000

    chore: add traits exmaple

--- rust/ex-traits/src/main.rs
+++ rust/ex-traits/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Exercise- Traits and Generics");
+    println!("{}","-".repeat(20));
+    // Traits are rust's answer to interfaces, abstract base classes,
+    // operator overloading. In C++, These are equivalent to virtual functions, CRTP,
+    // and concepts. For C developers, traits are structured way to do polymorphism
+
+    
 }
